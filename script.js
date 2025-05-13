25b8787038f0c15899af9de372457da2bd1ff71e
Janani0511 <jananiappanabhotla205@gmail.com>
2025-05-13 22:45:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple script to alert when clicking contact
+document.querySelector('#contact').addEventListener('click', () => {
+    alert('Thanks for visiting! You can reach me via email 💌');
+});
